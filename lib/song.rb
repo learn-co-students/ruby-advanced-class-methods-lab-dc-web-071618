0ad1c025fbff05ad51bf2b10b28c8affa9e84ae3
aubreeabril <aubree.abril@gmail.com>
2018-07-19 21:21:55 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song_name
   @@all = []
 
   def self.all
@@ -10,4 +10,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect do |songs|
+      songs.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |org_songs|
+      org_songs.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(' - ')
+    artist_name = parts[0]
+    song_part = parts[1].split('.')
+    song_name = song_part[0]
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(' - ')
+    artist_name = parts[0]
+    song_part = parts[1].split('.')
+    song_name = song_part[0]
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
